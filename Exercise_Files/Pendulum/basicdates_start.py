9ec9e66f16d1b02fd0d4893c8197cfd7dbd9b50c
MICHAEL LAU <michaellau@MICHAELs-MacBook-Pro.local>
2020-12-27 18:53:38 -0800

    test

--- Exercise_Files/Pendulum/basicdates_start.py
+++ Exercise_Files/Pendulum/basicdates_start.py
@@ -5,15 +5,21 @@ import time
 import pendulum
 
 # TODO: create a new datetime using pendulum
-dt1 = pendulum.datetime(2020,7,28)
+dt1 = pendulum.datetime(2020,7,28,tz="America/New_York")
 print(dt1)
 print(isinstance(dt1,datetime))
-print(dt1.timezone.name)
+print(dt1.timezone)
 
 # TODO: convert the time to another time zone
 
+dt2= dt1.in_timezone("Europe/Paris")
+print(dt2)
+
 
 # TODO: create a new datetime using the now() function
+dt3 = pendulum.now("Europe/London")
+print(dt3)
+print(dt3.timezone.name)
 
 
 # TODO: Use the local function function
